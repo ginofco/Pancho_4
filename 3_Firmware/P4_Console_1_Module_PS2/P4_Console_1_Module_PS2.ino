edcec7d97413022b4c451ddc756c3c98de40c442
ginofco <ginofco.diy@gmail.com>
2025-12-10 19:18:58 -0300

    Adaptação do código do Console - Módulo PS2 - para receber estrutura de dados do controle PS2

--- 3_Firmware/P4_Console_1_Module_PS2/P4_Console_1_Module_PS2.ino
+++ 3_Firmware/P4_Console_1_Module_PS2/P4_Console_1_Module_PS2.ino
@@ -16,6 +16,14 @@ int error = 0;
 byte type = 0;
 byte vibrate = 0;
 
+struct Data_Package {
+  int button2_state;
+  int button3_state;
+  int button4_state;
+  int outputValue0;
+  int outputValue1;
+} data;
+
 void setup(){
  Serial.begin(57600);
  
@@ -24,23 +32,67 @@ void setup(){
  // Fim trecho novo - com Wire
    
  error = ps2x.config_gamepad(13,11,10,12, true, true);   //setup pins and settings:  GamePad(clock, command, attention, data, Pressures?, Rumble?) check for error
- 
  if(error == 0){
    Serial.println("Found Controller, configured successful");
-   Serial.println("Try out all the buttons, X will vibrate the controller, faster as you press harder;");
-   Serial.println("holding L1 or R1 will print out the analog stick values.");
-   Serial.println("Go to www.billporter.info for updates and to report bugs.");
  }
- 
   else if(error == 1)
    Serial.println("No controller found, check wiring, see readme.txt to enable debug. visit www.billporter.info for troubleshooting tips");
   else if(error == 2)
    Serial.println("Controller found but not accepting commands. see readme.txt to enable debug. Visit www.billporter.info for troubleshooting tips");
   else if(error == 3)
    Serial.println("Controller refusing to enter Pressures mode, may not support it. ");
+}
+
+void loop(){
+ if(error == 1) // No controller found
+  return; 
+ else { 
+  ps2x.read_gamepad(false, vibrate);          //read controller and set large motor to spin at 'vibrate' speed
+
+  data.button2_state = ps2x.Button(PSB_BLUE);
+  data.button3_state = ps2x.Button(PSB_PAD_UP);
+  data.button4_state = ps2x.Button(PSB_PAD_DOWN);
+  data.outputValue0  = ps2x.Analog(PSS_RX);
+  data.outputValue1  = ps2x.Analog(PSS_RY);
+
+  Serial.print("Transmissor. Mensagem: luz: ");
+  Serial.print(data.button2_state);
+  Serial.print(" - direcao1: ");
+  Serial.print(data.button3_state);
+  Serial.print(" - direcao2:");
+  Serial.print(data.button4_state);
+  Serial.print(" - volante: ");
+  Serial.print(data.outputValue0);
+  Serial.print(" - aceler: ");
+  Serial.print(data.outputValue1);
+  Serial.println(" * ");
+
+  // Segue trecho novo - com Wire
+  Wire.beginTransmission(8); // transmit to device #8
+  // Central de decisao
+  state = 0;  // Default
+  if (ps2x.NewButtonState(PSB_BLUE)) state = 1;
+  Wire.write(state);         // sends one byte
+  Wire.endTransmission();    // stop transmitting
+  // Fim trecho novo - com Wire
+ }
  
+ delay(50);   
+}
+
+
+//////////////////////////////////////////
+// CÓDIGO ORIGINAL ///////////////////////
+//////////////////////////////////////////
+// EM "setup()"
+/*
+ if(error == 0){
+   Serial.println("Found Controller, configured successful");
+   Serial.println("Try out all the buttons, X will vibrate the controller, faster as you press harder;");
+   Serial.println("holding L1 or R1 will print out the analog stick values.");
+   Serial.println("Go to www.billporter.info for updates and to report bugs.");
+ }
    //Serial.print(ps2x.Analog(1), HEX);
-   
    type = ps2x.readType(); 
      switch(type) {
        case 0:
@@ -53,24 +105,19 @@ void setup(){
          Serial.println("GuitarHero Controller Found");
        break;
      }
-}
+*/
 
-void loop(){
-   /* You must Read Gamepad to get new values
+// EM "loop()"
+/*
+   You must Read Gamepad to get new values
    Read GamePad and set vibration values ps2x.read_gamepad(small motor on/off, larger motor strenght from 0-255)
    if you don't enable the rumble, use ps2x.read_gamepad(); with no values    you should call this at least once a second
-   */
-   
- if(error == 1) //skip loop if no controller found
-  return; 
- else { //DualShock Controller
-  ps2x.read_gamepad(false, vibrate);          //read controller and set large motor to spin at 'vibrate' speed
-    
+*/ 
+/*
   if(ps2x.Button(PSB_START))                   //will be TRUE as long as button is pressed
     Serial.println("Start is being held");
   if(ps2x.Button(PSB_SELECT))
     Serial.println("Select is being held");
-         
   if(ps2x.Button(PSB_PAD_UP)) {         //will be TRUE as long as button is pressed
     Serial.print("Up held this hard: ");
     Serial.println(ps2x.Analog(PSAB_PAD_UP), DEC);
@@ -86,8 +133,7 @@ void loop(){
   if(ps2x.Button(PSB_PAD_DOWN)){
     Serial.print("DOWN held this hard: ");
     Serial.println(ps2x.Analog(PSAB_PAD_DOWN), DEC);
-  }   
-  
+  }
   vibrate = ps2x.Analog(PSAB_BLUE);        //this will set the large motor vibrate speed based on 
                                               //how hard you press the blue (X) button    
   if (ps2x.NewButtonState())               //will be TRUE if any button changes state (on to off, or off to on)
@@ -103,7 +149,6 @@ void loop(){
       if(ps2x.Button(PSB_GREEN))
         Serial.println("Triangle pressed");
   }
-    
   if(ps2x.ButtonPressed(PSB_RED))             //will be TRUE if button was JUST pressed
     Serial.println("Circle just pressed");
          
@@ -112,7 +157,6 @@ void loop(){
     
   if(ps2x.NewButtonState(PSB_BLUE))            //will be TRUE if button was JUST pressed OR released
     Serial.println("X just changed");
-    
   if(ps2x.Button(PSB_L1) || ps2x.Button(PSB_R1)) // print stick values if either is TRUE
     {
       Serial.print("Stick Values:");
@@ -124,17 +168,33 @@ void loop(){
       Serial.print(",");
       Serial.println(ps2x.Analog(PSS_RX), DEC); 
   }   
- }
- 
- // Segue trecho novo - com Wire
- Wire.beginTransmission(8); // transmit to device #8
- // Central de decisao
- state = 0;  // Default
- if (ps2x.NewButtonState(PSB_BLUE)) state = 1;
- Wire.write(state);         // sends one byte
- Wire.endTransmission();    // stop transmitting
- // Fim trecho novo - com Wire
- 
- delay(50);
-     
-}
+*/
+
+// CÓDIGO ALTERNATIVO // Isadora
+/*
+  if(ps2x.Button(PSB_BLUE)) {
+   Serial.print("Transmissor. Mensagem: luz: ");
+   Serial.println("BLUE pressed");
+   data.button2_state = 1;
+  }
+  if(ps2x.Button(PSB_PAD_UP)) {
+   Serial.println("PAD_UP pressed");
+   data.button3_state = 1;
+  }
+  if(ps2x.Button(PSB_PAD_DOWN)) {
+   Serial.println("PAD_DOWN pressed");
+   data.button4_state = 1;
+  }
+  
+  if(ps2x.Button(PSB_L1) || ps2x.Button(PSB_R1)) { // print stick values if either is TRUE
+   data.outputValue0 = ps2x.Analog(PSS_RY);
+   Serial.print(ps2x.Analog(PSS_RY), DEC);
+   Serial.print(",");
+   Serial.print( data.outputValue0 ); 
+   Serial.print(",");
+   data.outputValue1 = ps2x.Analog(PSS_RX);
+   Serial.print( data.outputValue1 ); 
+   Serial.print(",");
+   Serial.println(ps2x.Analog(PSS_RX), DEC); 
+  } 
+*/        
