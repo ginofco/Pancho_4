a2175be5e525b59bd40997786e36ab4a6cc67506
ginofco <ginofco.diy@gmail.com>
2026-04-29 18:52:22 -0300

    Aumentando a estrutura de dados do Console

--- 3_Firmware/P4_Console_1_Module_PS2/P4_Console_1_Module_PS2.ino
+++ 3_Firmware/P4_Console_1_Module_PS2/P4_Console_1_Module_PS2.ino
@@ -10,12 +10,47 @@
 byte state = 0; // Robot state:  (Variavel global usada para comunicacao i2c.)
 // Definir o endereço I2C do escravo
 #define SLAVE_ADDRESS 0x08
-// Fim trecho novo - com Wire
 
 PS2X ps2x; // create PS2 Controller Class
 int error = 0; 
 byte type = 0;
 byte vibrate = 0;
+
+struct NewDataPackage {
+  boolean ps2_PSB_SELECT;  // buttons pressed, released or changed state
+  boolean ps2_PSB_L3;
+  boolean ps2_PSB_R3;
+  boolean ps2_PSB_START;
+  boolean ps2_PSB_PAD_UP;
+  boolean ps2_PSB_PAD_RIGHT;
+  boolean ps2_PSB_PAD_DOWN;
+  boolean ps2_PSB_PAD_LEFT;
+  boolean ps2_PSB_L2;
+  boolean ps2_PSB_R2;
+  boolean ps2_PSB_L1;
+  boolean ps2_PSB_R1;
+  boolean ps2_PSB_GREEN;
+  boolean ps2_PSB_RED;
+  boolean ps2_PSB_BLUE;
+  boolean ps2_PSB_PINK;
+  byte    ps2_PSS_RX;  // "Sticks" values
+  byte    ps2_PSS_RY;
+  byte    ps2_PSS_LX;
+  byte    ps2_PSS_LY;
+  byte    ps2_PSAB_PAD_RIGHT; // How strong pressed
+  byte    ps2_PSAB_PAD_UP;
+  byte    ps2_PSAB_PAD_DOWN;
+  byte    ps2_PSAB_PAD_LEFT;
+  byte    ps2_PSAB_L2;
+  byte    ps2_PSAB_R2;
+  byte    ps2_PSAB_L1;
+  byte    ps2_PSAB_R1;
+  byte    ps2_PSAB_GREEN;
+  byte    ps2_PSAB_RED;
+  byte    ps2_PSAB_BLUE;
+  byte    ps2_PSAB_PINK;
+} newdataToSend;
+
 
 struct DataPackage {
   int button2_state;
@@ -31,8 +66,10 @@ void setup(){
  // Segue trecho novo - com Wire
  Wire.begin(); // join i2c bus (address optional for master)
  // Fim trecho novo - com Wire
+ 
  error = ps2x.config_gamepad(13,11,10,12, true, true);   
- //setup pins and settings:  GamePad(clock, command, attention, data, Pressures?, Rumble?) 
+ 
+ //setup pins and settings:  GamePad(clock, command, attention, data, Pressures?, Rumble?)
  //check for error
  if(error == 0) Serial.println("Found Controller, configured successful");
   else if(error == 1) Serial.println("No controller found, check wiring, ");
